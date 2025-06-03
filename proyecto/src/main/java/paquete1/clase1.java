48ec3eed7e54a0f1102a333b84fa56032da79b3c
Antonio Gallardo <agg860@inlumine.ual.es>
2025-06-03 19:14:46 +0200

    Esto es una clase de verdad

--- proyecto/src/main/java/paquete1/clase1.java
+++ proyecto/src/main/java/paquete1/clase1.java
@@ -1,19 +1,28 @@
 package paquete1;
 
 public class clase1 {
+	
+	
+	public int suma(int num1, int num2) {
+		
+		return num1 + num2; 
+	}
+	
+
+	public int resta(int num1, int num2) {
+		
+		return num1 - num2; 
+	}
+
 
 	public static void main(String[] args) {
 
-		System.out.println("Clase 1");
-
-		System.out.println("Clase 1");
-		System.out.println("Clase 1");
-		System.out.println("Paco el Golondrina");
-		System.out.println("Paco el Golondrina");
-
+		
+		
+		
 
 
 
 	}
 
-}
+} 
