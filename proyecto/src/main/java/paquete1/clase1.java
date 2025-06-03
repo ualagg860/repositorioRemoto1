3413f1c9c3fb44e3a4a43e409e87704257f05377
Antonio Gallardo <agg860@inlumine.ual.es>
2025-06-03 18:56:14 +0200

    Reapply "Arreglados los conflictos"
    This reverts commit e4a3ac5b7f6bf2f18cca47fa4c726e0f28cc2178.

--- proyecto/src/main/java/paquete1/clase1.java
+++ proyecto/src/main/java/paquete1/clase1.java
@@ -5,9 +5,11 @@ public class clase1 {
 	public static void main(String[] args) {
 
 		System.out.println("Clase 1");
+
 		System.out.println("Clase 1");
 		System.out.println("Clase 1");
 		System.out.println("Paco el Golondrina");
+
 
 
 	}
